e10e60fd8e9cd7a297347ff7343a2dcb144b6b93
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-02-20 15:44:57 +0700

    Init

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private Rigidbody2D rb;
+        private Animator animator;
+
+        [SerializeField]
+        private float speed;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            float y = Input.GetAxisRaw("Vertical");
+            float x = Input.GetAxisRaw("Horizontal");
+
+            rb.velocity = new Vector2(x * speed, y * speed);
+
+            if (y == 0 && x == 0)
+            {
+                animator.SetFloat("Speed", 0);
+            }
+            else
+            {
+                animator.SetFloat("X", x);
+                animator.SetFloat("Y", y);
+                animator.SetFloat("Speed", 1);
+            }
+        }
+    }
+}
